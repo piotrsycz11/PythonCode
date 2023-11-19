98d54deb9512c4e3c519c0b65bc05293e78b2138
piotrsycz11 <piotr.sycz11@gmail.com>
2023-11-19 14:30:41 +0100

    Update user_password.py

--- user_password.py
+++ user_password.py
@@ -2,8 +2,7 @@
 import random
 import string
 
-# passwords = {}
-
+passwords = {}
 class User:
     def __init__(self, name, surname):
         self.name = name
@@ -26,9 +25,13 @@ class User:
                 new_password = input("Type new password: ")
                 second_password = input("Type new password again: ")
                 if new_password == second_password:
-                    if len(second_password) >= 10 and any(second_password.isupper()) and any(second_password.isdigit()) and any(second_password in "!@#$%^&*()"):
+                    if (len(second_password) >= 6 and 
+                        any(char.isupper() for char in second_password) and 
+                        any(char.isdigit() for char in second_password) and 
+                        any(char in "!@#$%^&*()" for char in second_password)):
                         passwords[self.username] = new_password
                         print("Password has been changed.")
+                        return
                     else:
                         print("Password is not safe enough.")
                         return
@@ -47,38 +50,68 @@ class User:
                 self.modify_password()
             return True
         return False
+    
+    def delete_user(self):
+        if self.username in passwords:
+            ans = input("Input admin password: ")
+            if ans.lower() == 'admin':
+                del passwords[self.username]
+                print("User has been deleted.")
+                return
+        else:
+            print("Username does not exist.")
+            return
 
 def initials():
     return input("Name: "), input("Surname: ")
-def save_to_csv():
-    with open("passwords.csv", "a") as file:
-        for key, value in passwords.items():
-            file.write(f"{key},{value}\n")
+def save_to_csv(passwords): 
+    existing_users = {}
+    try:
+        with open("passwords.csv", "r") as file:
+            for line in file:
+                key, value = line.strip().split(",")
+                existing_users[key] = value
+    except FileNotFoundError:
+        pass
+    if len(passwords) >= len(existing_users):
+        existing_users.update(passwords)
+        with open("passwords.csv", "w") as file:
+            for key, value in existing_users.items():
+                file.write(f"{key},{value}\n")
+    else:
+        existing_users = passwords
+        with open("passwords.csv", "w") as file:
+            for key, value in existing_users.items():
+                file.write(f"{key},{value}\n")
+
 while True:
     with open("passwords.csv", "r") as file:
-        passwords = {}
         for _ in file:
             key, value = _.strip().split(",")
             passwords[key] = value
-        print(passwords)
-    action = input("Choose an action: [generate/modify/see/exit] ").lower()
+    action = input("Choose an action: [generate/modify/delete/see/exit] ").lower()
     if action == 'generate':
         name, surname = initials()
         user = User(name, surname)
         if not user.check_existing():
             password = user.password_generator()
-            save_to_csv()
+            save_to_csv(passwords)
             print(f"Username: {user.username} Password: {password}")
     elif action == 'modify':
         name, surname = initials()
         user = User(name, surname)
         user.modify_password()
-        save_to_csv()
+        save_to_csv(passwords)
     elif action == 'see':
         admin = input("Type admin password: ")
         if admin == "admin":
             with open("passwords.csv", "r") as file:
                 print(file.read())
+    elif action == 'delete':
+        name, surname = initials()
+        user = User(name, surname)
+        user.delete_user()
+        save_to_csv(passwords)
     elif action == 'exit':
-        save_to_csv()
+        save_to_csv(passwords)
         break
